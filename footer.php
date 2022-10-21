77f1d502f570a9327bb21a0ef56149da076b697d
Nicktaechan <nickn7500@gmail.com>
2022-10-21 14:26:52 +0300

    new commit

---
+++ footer.php
@@ -0,0 +1,42 @@
+<section class="footer">
+
+   <div class="box-container">
+
+      <div class="box">
+         <h3>quick links</h3>
+         <a href="home.php">Home</a>
+         <a href="about.php">About us</a>
+         <a href="courses.php">Courses</a>
+         <a href="contact.php">Contact</a>
+      </div>
+
+      <div class="box">
+         <h3>extra links</h3>
+         <a href="login.php">Login</a>
+         <a href="register.php">Register</a>
+         <a href="gallery.php">Gallery</a>
+         <a href="dashboard.php">Dashboard</a>
+      </div>
+
+      <div class="box">
+         <h3>contact info</h3>
+         <p> <i class="fas fa-phone"></i> 0721293435 </p>
+         <p> <i class="fas fa-phone"></i> 0721293435 </p>
+         <p> <i class="fas fa-envelope"></i> stayup1234@gmail.com </p>
+         <p> <i class="fas fa-envelope"></i> stayup@gmail.com </p>
+         <p> <i class="fas fa-map-marker-alt"></i> Rongai, Kajiado - 00511 </p>
+      </div>
+
+      <div class="box">
+         <h3>follow us</h3>
+         <a href="#"> <i class="fab fa-facebook-f"></i> Facebook </a>
+         <a href="#"> <i class="fab fa-twitter"></i> Twitter </a>
+         <a href="#"> <i class="fab fa-instagram"></i> Instagram </a>
+         <a href="#"> <i class="fab fa-linkedin"></i> Linkedin </a>
+      </div>
+
+   </div>
+
+   <p class="credit"> &copy; copyright  @ <?php echo date('Y'); ?> by <span>nicksoncompanies</span> </p>
+
+</section>
